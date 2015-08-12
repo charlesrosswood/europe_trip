7cb75fc895be337149ae31838ab349d9e86e9b3d
Charles Wood <charles.ross.wood@gmail.com>
2015-08-12 11:38:05 +0100

    Fixing rendering of post

--- static/js/view_saved/main.js
+++ static/js/view_saved/main.js
@@ -80,9 +80,11 @@ function buildPostForm(params, postKey) {
   var miniGoogleMap = buildMiniMap(params);
   var miniMapNode = miniGoogleMap.container;
 
-  var imgFileNames = params.fileNames.join(', ');
-  var imgFileNamesNode = createNodeWithContent('div', imgFileNames);
-  addClass(imgFileNamesNode, 'filenames');
+  if (params.files) {
+    var imgFileNames = params.fileNames.join(', ');
+    var imgFileNamesNode = createNodeWithContent('div', imgFileNames);
+    addClass(imgFileNamesNode, 'filenames');
+  }
 
   var fileUploadNode = document.createElement('input');
   fileUploadNode.setAttribute('type', 'file');
@@ -100,7 +102,11 @@ function buildPostForm(params, postKey) {
   postContainer.appendChild(titleNode);
   postContainer.appendChild(statusTextNode);
   postContainer.appendChild(miniMapNode);
-  postContainer.appendChild(imgFileNamesNode);
+
+  if (params.files) {
+    postContainer.appendChild(imgFileNamesNode);
+  }
+  
   postContainer.appendChild(fileUploadNode);
   postContainer.appendChild(deletePost);
   return {
