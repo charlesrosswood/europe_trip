8b2bf61202ec28dbbf7bea52160b26cf67185e6f
Charles Wood <charles.ross.wood@gmail.com>
2015-08-12 12:51:48 +0100

    Fixing bouncing scrolling

--- static/js/base_view/main.js
+++ static/js/base_view/main.js
@@ -27,3 +27,28 @@ navPane.addEventListener('click', function(event) {
     }
   }
 });
+
+// preventing bounce on mobile scrolling
+var selScrollable = '.scrollable';
+
+// Uses document because document will be topmost level in bubbling
+document.addEventListener('touchmove',function(e){
+  e.preventDefault();
+});
+
+// Uses body because jQuery on events are called off of the element they are
+// added to, so bubbling would not work if we used document instead.
+document.getElementsByTagName('body')[0].addEventListener('touchstart', selScrollable,
+  function(e) {
+    if (e.currentTarget.scrollTop === 0) {
+      e.currentTarget.scrollTop = 1;
+    } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+      e.currentTarget.scrollTop -= 1;
+    }
+  }
+);
+
+// Stops preventDefault from being called on document if it sees a scrollable div
+document.getElementsByTagName('body')[0].addEventListener('touchmove', selScrollable, function(e) {
+  e.stopPropagation();
+});
